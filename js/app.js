b6d59ed5e63ffb1502304155d65ba59842f9dba1
Sacha Ponniah <sponniah812@gmail.com>
2021-12-20 14:24:58 +0000

    MVP and extensions complete.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript loaded')
+    console.log('JavaScript loaded');
+
+    const form = document.querySelector('#new-cuddly-form');
+    form.addEventListener('submit', handleFormSubmit);
+
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handleDeleteAll);
 })
+
+const handleFormSubmit = function(event) {
+    event.preventDefault();
+    console.log(event)
+    
+    const cuddlyDirectoryItem = createCuddlyDirectoryItem(event.target); 
+    const cuddlyDirectory = document.querySelector('#cuddly-directory');
+    cuddlyDirectory.appendChild(cuddlyDirectoryItem);
+
+    event.target.reset();
+}
+
+const createCuddlyDirectoryItem = function(form) {
+    const cuddlyDirectoryItem = document.createElement('li');
+    cuddlyDirectoryItem.classList.add('cuddly-directory-item');
+
+    const name = document.createElement('h2');
+    name.textContent = `Name: ${form.name.value}`;
+    cuddlyDirectoryItem.appendChild(name);
+
+    const species = document.createElement('h3');
+    species.textContent = `Species: ${form.species.value}`;
+    cuddlyDirectoryItem.appendChild(species);
+
+    const owner = document.createElement('p');
+    owner.textContent = `Owner: ${form.owner.value}`;
+    cuddlyDirectoryItem.appendChild(owner);
+
+    const cleaning = document.createElement('p');
+    cleaning.textContent = `Cleaning: ${form.cleaning.value}`;
+    cuddlyDirectoryItem.appendChild(cleaning);
+
+    return cuddlyDirectoryItem;
+}
+
+const handleDeleteAll = function() {
+    const cuddlyDirectory = document.querySelector('#cuddly-directory');
+    cuddlyDirectory.innerHTML = '';
+}
